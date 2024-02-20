9c94776dc31ee52ce214bd6c0f57bfc37e5bf44e
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-02-20 23:52:05 +0700

    upcode

---
+++ src/components/pages/PhieuMuon/DocGiaInfoview.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="card mt-3">
+    <div class="card-header">
+      <h6>Thông tin chi tiết</h6>
+    </div>
+    <div class="card-body">
+      <div class="form-group">
+        <label>Họ và tên</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="_model.name"
+          readonly
+        />
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="_model.email"
+          readonly
+        />
+      </div>
+      <div class="form-group">
+        <label>Địa chỉ</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="_model.diaChi"
+          readonly
+        />
+      </div>
+      <div class="form-group">
+        <label>Ngày sinh</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="_model.ngaySinh"
+          readonly
+        />
+      </div>
+      <div class="form-group">
+        <label>Hạn sử dụng thẻ</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="_model.hanSuDung"
+          readonly
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import docgiavm from "../../../model/docgia/docgiavm";
+
+defineProps<{
+    _model: docgiavm;
+}>();
+</script>
+<style scoped lang="scss"></style>
---
+++ src/components/pages/PhieuMuon/SachInfoview.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card mt-3">
+    <div class="card-header">
+      <h6>Thông tin chi tiết</h6>
+    </div>
+    <div class="card-body">
+      <div
+        class="alert alert-secondary"
+        v-if="_arrylist"
+        v-for="item in _arrylist"
+        :key="item.id"
+      >
+        <div class="alert-body">
+          <button
+            class="close text-danger"
+            data-dismiss="alert"
+            @click="deleteitem(item.id)"
+          >
+            <span>×</span>
+          </button>
+          Sách:&nbsp;<span class="text-info text-size">{{
+            item.name || "No book"
+          }}</span
+          >&nbsp;&nbsp; Số lượng:&nbsp;<span class="text-info text-size"
+            >1</span
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import sachvm from '../../../model/sach/sachvm';
+
+defineProps<{
+    _arrylist: Array<sachvm>;
+}>();
+
+const emit = defineEmits<{
+  (e: 'deleteitem', id: string): void
+}>();
+const deleteitem = (id: string) => {
+    emit("deleteitem",id);
+};
+</script>
+<style scoped lang="scss"></style>
